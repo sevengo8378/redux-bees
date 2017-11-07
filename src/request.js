259fefd0e76099fc7baedf169fad0697a70ee7c9
byrne.liang <byrne.liang@alo7.com>
2017-11-07 17:40:47 +0800

    fix content-type

--- src/request.js
+++ src/request.js
@@ -21,7 +21,7 @@ export default function request(baseUrl, path, options) {
         url: res.url
       };
 
-      const contentType = headers.content-type || headers.Content-Type;
+      const contentType = headers['content-type'] || headers['Content-Type'];
       const isJsonType = contentType && contentType.includes('json')
 
       if (res.status !== 204 && isJsonType) {
